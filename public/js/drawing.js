26bdadc3c3162d6d6bdb74799cf851f9e17262be
Mishal Islam <mishal.islam01@gmail.com>
2014-12-26 14:13:41 +0000

    user can click buttons to change colour

--- public/js/drawing.js
+++ public/js/drawing.js
@@ -1,18 +1,19 @@
+// var pickColour = require('./pickColour.js');
+
 var context = $("canvas")[0].getContext("2d");
 var lastEvent;
+var colour = "black";
 
 $(document).ready(function(){
-  drawWhenMouseClicked()
+  drawWhenMouseClicked();
+  pickColour();
 });
 
 function drawWhenMouseClicked(){
   $("canvas").mousedown(function(e){
-    lastEvent = e;
-    mouseDown = true;
+    mouseClicked(e)
   }).mousemove(function(e){
-  if(mouseDown) {
-    draw(context, e)
-  }
+    mouseDrag(mouseDown, context, e)
   }).mouseup(function(){
     mouseDown = false;
   }).mouseleave(function(){
@@ -20,11 +21,28 @@ function drawWhenMouseClicked(){
   });
 };
 
+function mouseClicked(e){
+  lastEvent = e;
+  mouseDown = true;
+};
+
+function mouseDrag(mouseDown, context, e){
+  if(mouseDown) {
+    draw(context, e)
+  }
+};
+
 function draw(context, e){
   context.beginPath();
   context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
   context.lineTo(e.offsetX, e.offsetY);
-  context.strokeStyle = "black";
+  context.strokeStyle = colour;
   context.stroke();
   lastEvent = e;
+};
+
+function pickColour(){
+  $("button").click(function(){
+    colour = this.id
+  });
 };
